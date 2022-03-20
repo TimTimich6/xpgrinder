873dcee4cb84b6ddb25722c14468a1e215c266cd
Tim <timofeyobraztsov@gmail.com>
2022-03-20 02:16:22 -0700

    changed cors

--- backend/src/serverside/server.ts
+++ backend/src/serverside/server.ts
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const port: number | string = process.env.port || 3080;
 
 interface WebSocketStorage {
