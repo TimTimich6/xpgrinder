b3a547dc469f3e36a0e5c4e8b3f8a53fbb36cfe8
Tim <timofeyobraztsov@gmail.com>
2022-05-22 15:03:09 -0700

    filters download/upload

--- client/src/ServerList/CreateFilter.jsx
+++ client/src/ServerList/CreateFilter.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { useContext, useState } from "react";
+import { ServerListContext } from "../ServerListContext";
 import TextButton from "../TextButton";
 import { UserSettingsContext } from "../UserSettingsContext";
 const CreateFilter = (props) => {
   const [filterInput, setFilterInput] = useState("");
   const [responseInput, setResponseInput] = useState("");
   const { setError, setLoading } = useContext(UserSettingsContext);
+  const { servers } = useContext(ServerListContext);
+
   const { setServers, currentServer } = props;
   const addFilter = async (filter) => {
     if (!filter.filter || !filter.response) {
@@ -51,6 +54,41 @@ const CreateFilter = (props) => {
       });
     });
   };
+
+  const downloadFilters = () => {
+    if (servers[currentServer] && servers[currentServer].filters && servers[currentServer].filters.length > 0) {
+      console.log(servers[currentServer]);
+      const filters = servers[currentServer].filters;
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.setAttribute("href", "data:text/plain;charset=utf-8, " + encodeURIComponent(JSON.stringify(filters)));
+      a.download = `filters${servers[currentServer].name}.json`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+
+  const uploadFilters = () => {
+    const a = document.querySelector("#filterupload");
+    a.click();
+  };
+  const uploaded = async (e) => {
+    try {
+      const text = await e.target.files[0].text();
+      if (text) {
+        const parsed = JSON.parse(text);
+        setServers((prevState) => {
+          return prevState.map((server, i) => {
+            if (i === currentServer) return { ...server, filters: parsed };
+            return server;
+          });
+        });
+      } else throw new Error();
+    } catch (error) {
+      setError({ title: "Upload error", description: "Unexpected error occured when uploading a file" });
+    }
+  };
   return (
     <>
       <div className="filterTotal">
@@ -61,7 +99,7 @@ const CreateFilter = (props) => {
           <input onChange={(e) => setResponseInput(e.target.value)} type="text" required className="flText" value={responseInput} />
         </div>
       </div>
-      <div className="filtersButtons">
+      <div className="buttonsServer">
         <div className="buttonWrapper" onClick={() => addFilter({ filter: filterInput, response: responseInput })}>
           <TextButton bgc="rgb(23, 149, 118)">Create New Filter</TextButton>
         </div>
@@ -70,6 +108,11 @@ const CreateFilter = (props) => {
         </div>
         <div className="buttonWrapper" onClick={() => clearFilters()}>
           <TextButton bgc="#BB3C3C">Clear All</TextButton>
+        </div>
+        <div className="downloadbuttons">
+          <img src="./downloadlogo.png" alt="" className="forcePower" data-tip="Download filters to a json file" onClick={downloadFilters} />
+          <img src="./uploadlogo.png" alt="" className="forcePower" data-tip="Upload filters from json file" onClick={uploadFilters} />
+          <input type="file" id="filterupload" name="avatar" style={{ display: "none" }} accept="application/JSON" onChange={(e) => uploaded(e)} />
         </div>
       </div>
     </>
